c10c3e442fd1b739e525b1ba84383263e2839b18
yangshilei <17751693884@163.com>
2018-11-30 14:54:00 +0800

    vue项目脚手架模板

---
+++ myproject/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>好好学习，天天向上</h1>
+    <!--<img src="./assets/logo.png">-->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
